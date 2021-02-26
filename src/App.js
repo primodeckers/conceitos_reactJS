2403be043e6866667fbb143ce1f7ae5fe483b952
Renê Estevam Deckers <primodeckers@hotmail.com>
2021-02-26 19:07:30 -0300

    Desafio reactjs

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const { data } = await api.post('repositories', {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    });
+
+    setRepositories([...repositories, data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {
+          repositories.map(repository => 
+            <li key={repository.id}>
+              {repository.title}
+
+              <button type="button" onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        }
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
